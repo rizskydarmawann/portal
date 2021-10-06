860d4ba78474a1fd6c14039da02331fb14b5f0c4
rizskydarmawann <70370415+rizskydarmawann@users.noreply.github.com>
2021-10-07 00:52:22 +0700

    Update dashboard.php

--- application/views/admin/home/dashboard.php
+++ application/views/admin/home/dashboard.php
@@ -1,7 +1,7 @@
   <!-- Begin Page Content -->
   <div class="container-fluid">
             <!-- Page Heading -->
-            <h5 class="h3 mb-4 text-gray-800">Selamatt datang di dashboard <?=$this->config->item('applicationName')?></h5>
+            <h5 class="h3 mb-4 text-gray-800">Selamat datang di dashboard <?=$this->config->item('applicationName')?></h5>
 
 
         </div>
