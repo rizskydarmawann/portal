d13ac16e5990bf49d483162fa07ada1474ab18dd
unknown <muhammad45rifki@gmail.com>
2021-10-14 18:25:07 +0700

    update db

--- portal.sql
+++ portal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 14, 2021 at 11:09 AM
+-- Generation Time: Oct 14, 2021 at 01:24 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 7.3.28
 
